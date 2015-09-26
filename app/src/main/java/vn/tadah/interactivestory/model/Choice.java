d343c7a59c44eb952c3bcafcf4a4a40a71640559
Kha Chuong <khachuongvn@hotmail.com>
2015-09-26 15:34:44 +0700

    Initial commit

---
+++ app/src/main/java/vn/tadah/interactivestory/model/Choice.java
@@ -0,0 +1,28 @@
+package vn.tadah.interactivestory.model;
+
+/**
+ * Created by VietNam on 9/26/15.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+    public String getmText() {
+        return mText;
+    }
+
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
+
+    public int getmNextPage() {
+        return mNextPage;
+    }
+
+    public void setmNextPage(int mNextPage) {
+        this.mNextPage = mNextPage;
+    }
+}
---
+++ app/src/main/java/vn/tadah/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,84 @@
+package vn.tadah.interactivestory.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import vn.tadah.interactivestory.R;
+import vn.tadah.interactivestory.model.Page;
+import vn.tadah.interactivestory.model.Story;
+
+public class StoryActivity extends AppCompatActivity {
+
+    private String mName;
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private Page mCurrentPage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+        Intent intent = getIntent();
+        mName = intent.getStringExtra(getString(R.string.key_name));
+        Toast.makeText(this, mName, Toast.LENGTH_SHORT).show();
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+        loadPage(0);
+
+    }
+    private void loadPage(int choice){
+        mCurrentPage = mStory.getPage(choice);
+        Context mContext = getApplicationContext();
+        Drawable drawable = ContextCompat.getDrawable(mContext, mCurrentPage.getImageId());
+        mImageView.setImageDrawable(drawable);
+        String pageText = mCurrentPage.getText();
+        pageText = String.format(pageText, mName);
+
+        mTextView.setText(pageText);
+        if(mCurrentPage.isFinal()){
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("Play again!");
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }else{
+            mChoice1.setText(mCurrentPage.getChoice1().getmText());
+            mChoice2.setText(mCurrentPage.getChoice2().getmText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice1().getmNextPage();
+                    loadPage(nextPage);
+                }
+            });
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice2().getmNextPage();
+                    loadPage(nextPage);
+                }
+            });
+        }
+
+
+
+    }
+
+}
